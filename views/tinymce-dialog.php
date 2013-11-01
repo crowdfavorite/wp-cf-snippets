d4597abaac03aa24f66addd9bcf7b70baf45cace
Steven Mathias <smathias@crowdfavorite.com>
2013-11-01 15:59:00 -0600

    Updated missed .live call.

--- views/tinymce-dialog.php
+++ views/tinymce-dialog.php
@@ -7,7 +7,7 @@
 		<script type="text/javascript">
 			;(function($) {
 				$(function() {
-					$(".cfsp-list-link").live('click', function(e) {
+					$("body").on("click", ".cfsp-list-link", function(e) {
 						var key = $(this).attr('rel');
 						cfsp_insert(key);
 						e.preventDefault();
