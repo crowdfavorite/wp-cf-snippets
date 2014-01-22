72867437227cdcebb9527cb61a49eb8005ed086e
Justin de Vesine <justin@crowdfavorite.com>
2014-01-21 17:37:40 -0700

    move more resources out of cf-snippets

--- includes/class-cf-snippet-admin.php
+++ includes/class-cf-snippet-admin.php
@@ -10,6 +10,116 @@ class CF_Snippet_Admin extends CF_Snippet_Base {
 			return;
 		}
 
-
+		$this->add_actions();
 	}
+
+	/**
+	 * Register all admin-specific actions
+	 */
+	function add_actions() {
+		add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_resources'));
+		add_action('admin_menu', array($this, 'admin_menu'));
+		add_action('right_now_content_table_end', array($this, 'rightnow_end'));
+		add_action('cf_admin_rightnow', array($this, 'rightnow_cfadmin_end'));
+	}
+
+	/**
+	 * Called during admin_enqueue_scripts hook processing
+	 */
+	public function enqueue_admin_resources($hook = '') {
+		switch ($hook) {
+		case 'post-new.php':
+			// fallthrough
+		case 'post.php':
+			// Add the proper CSS/JS to the Post/Page/Custom Post Type Edit screen
+			wp_enqueue_script('cfsp-post-js', CFSP_DIR_URL . 'js/post.js', array('jquery'), CFSP_VERSION);
+			wp_enqueue_style('cfsp-post-css', CFSP_DIR_URL . 'css/post.css', array(), CFSP_VERSION, 'screen');
+			break;
+		case 'settings_page_cf-snippets':
+			// Add the proper CSS/JS to the Settings screen
+			// if (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) {
+			wp_enqueue_script('cfsp-admin-js', false, array('cfsp-admin-js-behavior', 'csfp-admin-js-domwindow', 'cfsp-admin-js-popup'), CFSP_VERSION);
+			wp_enqueue_script('cfsp-admin-js-behavior', CFSP_DIR_URL . 'js/behavior.js', array('jquery'), CFSP_VERSION);
+			wp_enqueue_script('cfsp-admin-js-domwindow', CFSP_DIR_URL . 'js/jquery.DOMWindow.js', array('cfsp-admin-js-behavior'), CFSP_VERSION);
+			wp_enqueue_script('cfsp-admin-js-popup', CFSP_DIR_URL . 'js/popup.js', array('cfsp-admin-js-domwindow'), CFSP_VERSION);
+			// } else { <concatenated resource> }
+
+			wp_enqueue_style('cfsp-admin-css', CFSP_DIR_URL . 'css/content.css', array(), CFSP_VERSION, 'screen');
+			break;
+		}
+	}
+
+	public function admin_menu() {
+		add_options_page(
+			__('CF Snippets', 'cfsp'),
+			__('CF Snippets', 'cfsp'),
+			'manage_options',
+			'cf-snippets',
+			array($this, 'options_page')
+		);
+		if (defined('CF_ADMIN_VER')) {
+			add_submenu_page(
+				'cf-admin-menu',
+				__('CF Snippets', 'cfsp'),
+				__('CF Snippets', 'cfsp'),
+				10,
+				'cf-snippets',
+				array($this, 'options_page')
+			);
+		}
+	}
+
+	function options_page() {
+		global $cf_snippet;
+		if (class_exists('CF_Snippet_Manager') && !($cf_snippet instanceof CF_Snippet_Manager)) {
+			$cf_snippet = new CF_Snippet_Manager();
+		}
+
+		$table_display = '';
+		$message_display = '';
+		$post_table_display = '';
+		$post_message_display = '';
+		$count = 0;
+		$post_count = 0;
+		$show_post_count = CFSP_SHOW_POST_COUNT;
+		$total_post_count = 0;
+		$total_post_page_count = 0;
+
+		$table_content = '';
+		$post_table_content = '';
+
+		$keys = $cf_snippet->get_keys();
+		if (is_array($keys) && !empty($keys)) {
+			foreach ($keys as $key) {
+				if ($cf_snippet->exists($key)) {
+					$table_content .= $cf_snippet->admin_display($key);
+					$count++;
+					$message_display = ' style="display:none;"';
+				}
+			}
+		}
+		include(CFSP_DIR . 'views/options.php');
+	}
+
+	/**
+	 * Add some information to the "Right Now" section of the WP Admin Dashboard.  This will make it easier to
+	 * get into the Snippets edit screen.
+	 */
+	function rightnow_end() {
+		if (!defined('CF_ADMIN_VER')) {
+			$cf_snippet = new CF_Snippet_Manager();
+			$count = count($cf_snippet->get_keys());
+			$link = admin_url('options-general.php?page=cf-snippets');
+			include(CFSP_DIR . 'views/admin-rightnow.php');
+		}
+	}
+
+	function rightnow_cfadmin_end() {
+		$cf_snippet = new CF_Snippet_Manager();
+		$count = count($cf_snippet->get_keys());
+		$link = admin_url('options-general.php?page=cf-snippets');
+		include(CFSP_DIR . 'views/admin-rightnow.php');
+	}
+
+
 }
--- includes/class-cf-snippet-core.php
+++ includes/class-cf-snippet-core.php
@@ -12,16 +12,26 @@ class CF_Snippet_Core extends CF_Snippet_Base {
 		$this->add_actions();
 
 		if (is_admin()) {
+			new CF_Snippet_Admin();
 			CF_Snippet_Upgrader::i()->add_actions();
 		}
 	}
 
+	/**
+	 * All actions needed for basic operation
+	 */
 	function add_actions() {
 		add_action('init', array($this, 'set_defines'), 1);
 		add_action('init', array($this, 'register_post_types'), 1);
-		add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_resources'));
 	}
 
+	/**
+	 * Define the CFSP_DIR_URL url to this plugin's top-level directory
+	 *
+	 * Since cf-snippets can be used either as a standalone plugin or baked into
+	 * a theme, the plugins_url call by itself is not a reliable method of
+	 * determining URL structure.
+	 */
 	public function set_defines() {
 		// Per http://codex.wordpress.org/Function_Reference/plugins_url this is called in init, not at plugin load
 		if (!defined('CFSP_DIR_URL')) {
@@ -31,29 +41,6 @@ class CF_Snippet_Core extends CF_Snippet_Base {
 			else {
 				define('CFSP_DIR_URL', trailingslashit(plugins_url(basename(CFSP_DIR))));
 			}
-		}
-	}
-
-	public function enqueue_admin_resources($hook = '') {
-		switch ($hook) {
-			case 'post-new.php':
-				// fallthrough
-			case 'post.php':
-				// Add the proper CSS/JS to the Post/Page/Custom Post Type Edit screen
-				wp_enqueue_script('cfsp-post-js', CFSP_DIR_URL . 'js/post.js', array('jquery'), CFSP_VERSION);
-				wp_enqueue_style('cfsp-post-css', CFSP_DIR_URL . 'css/post.css', array(), CFSP_VERSION, 'screen');
-				break;
-			case 'settings_page_cf-snippets':
-				// Add the proper CSS/JS to the Settings screen
-				// if (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) {
-				wp_enqueue_script('cfsp-admin-js', false, array('cfsp-admin-js-behavior', 'csfp-admin-js-domwindow', 'cfsp-admin-js-popup'), CFSP_VERSION);
-				wp_enqueue_script('cfsp-admin-js-behavior', CFSP_DIR_URL . 'js/behavior.js', array('jquery'), CFSP_VERSION);
-				wp_enqueue_script('cfsp-admin-js-domwindow', CFSP_DIR_URL . 'js/jquery.DOMWindow.js', array('cfsp-admin-js-behavior'), CFSP_VERSION);
-				wp_enqueue_script('cfsp-admin-js-popup', CFSP_DIR_URL . 'js/popup.js', array('cfsp-admin-js-domwindow'), CFSP_VERSION);
-				// } else { <concatenated resource> }
-
-				wp_enqueue_style('cfsp-admin-css', CFSP_DIR_URL . 'css/content.css', array(), CFSP_VERSION, 'screen');
-				break;
 		}
 	}
 
