af9b7be36756b84ff1ed42a1ecb897419f124a37
Justin de Vesine <justin@crowdfavorite.com>
2014-01-23 16:17:06 -0700

    move tinymce

--- cf-snippets.php
+++ cf-snippets.php
@@ -328,39 +328,6 @@ add_action('save_post', 'cfsp_save_post', 10, 2);
 
 ## JS/CSS Addition
 
-## TinyMCE Functionality
-
-function cfsp_dialog() {
-	global $cf_snippet;
-	if (class_exists('CF_Snippet') && !($cf_snippet instanceof CF_Snippet)) {
-		$cf_snippet = new CF_Snippet();
-	}
-	$list = $cf_snippet->list_display(true);
-	include('views/tinymce-dialog.php');
-}
-
-function cfsp_addtinymce() {
-	// Don't bother doing this stuff if the current user lacks permissions
-	if (!current_user_can('edit_posts') && !current_user_can('edit_pages')) { return; }
-
-	// Add only in Rich Editor mode
-	if (get_user_option('rich_editing') == 'true') {
-		add_filter("mce_external_plugins", "add_cfsnip_tinymce_plugin");
-		add_filter('mce_buttons', 'register_cfsnip_button');
-	}
-}
-add_action('init', 'cfsp_addtinymce');
-
-function register_cfsnip_button($buttons) {
-	array_push($buttons, '|', "cfsnip_Btn");
-	return $buttons;
-}
-
-function add_cfsnip_tinymce_plugin($plugin_array) {
-	$plugin_array['cfsnippets'] = CFSP_DIR_URL.'js/editor_plugin.js';
-	return $plugin_array;
-}
-
 ## WordPress Admin Help Addition
 
 function cfsp_admin_help() {
