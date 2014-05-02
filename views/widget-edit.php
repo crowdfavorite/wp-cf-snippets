2f5c46edff5744813410f55558c881a64fc1b884
Chris Mospaw <cmospaw@crowdfavorite.com>
2014-05-02 09:46:55 -0600

    Add esc_attr() to attributes.

--- views/widget-edit.php
+++ views/widget-edit.php
@@ -1,10 +1,10 @@
 <p>
-	<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'cfsp'); ?></label>
-	<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+	<label for="<?php echo esc_attr($this->get_field_id('title')); ?>"><?php _e('Title:', 'cfsp'); ?></label>
+	<input class="widefat" id="<?php echo esc_attr($this->get_field_id('title')); ?>" name="<?php echo esc_attr($this->get_field_name('title')); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
 </p>
 <p>
-	<label for="<?php echo $this->get_field_id('list_key'); ?>"><?php _e('Snippet: ', 'cfsp'); ?></label>
-	<select id="<?php echo $this->get_field_id('list_key'); ?>" name="<?php echo $this->get_field_name('list_key'); ?>" class="widefat">
+	<label for="<?php echo esc_attr($this->get_field_id('list_key')); ?>"><?php _e('Snippet: ', 'cfsp'); ?></label>
+	<select id="<?php echo esc_attr($this->get_field_id('list_key')); ?>" name="<?php echo esc_attr($this->get_field_name('list_key')); ?>" class="widefat">
 		<option value="0"><?php _e('--Select Snippet--', 'cfsp'); ?></option>
 		<?php echo $select; ?>
 	</select>
